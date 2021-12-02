8c3d91137b55940315339ab964a8deaa6b3b3cbd
RealNoody <95240778+RealNoody@users.noreply.github.com>
2021-12-03 02:00:48 +0300

    Add files via upload

--- Project/src/userInterface/MainController.java
+++ Project/src/userInterface/MainController.java
@@ -2,6 +2,8 @@ package userInterface;
 
 import java.io.IOException;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,8 +15,10 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
@@ -73,6 +77,8 @@ public class MainController {
 	private TextField tfEditStudentTeam;
 	@FXML
 	private TabPane tpCompetitions;
+	@FXML
+	private TableView<String> TV;
 
 	@FXML
 	void closeWindow(ActionEvent event) throws IOException {
@@ -117,7 +123,13 @@ public class MainController {
 		// TODO complete the CompetitionTab scene and try again
 		VBox root = FXMLLoader.load(getClass().getResource("CompetitionTab.fxml"));
 		Tab tab = new Tab("Competition1", root);
-		((Labeled)root.getChildren().get(0)).setText("Competetion1");
+//		((Labeled)root.getChildren().get(0)).setText("Competetion1");
+//		ObservableList<String> data = FXCollections.observableArrayList(
+//				"momo","2019","swe","1"
+//		);
+//		((TableView<String>) root.getChildren().get(1)).setEditable(true);
+//		((TableView<String>) root.getChildren().get(1)).setItems(data);
+//		((TableView<String>) root.getChildren().get(1)).refresh();
 		tpCompetitions.getTabs().add(tab);
 	}
 
