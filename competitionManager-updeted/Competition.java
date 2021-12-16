6d8a08f84f200d527f96fd0bd32391fa8e0451cb
omar12al1234 <62028638+omar12al1234@users.noreply.github.com>
2021-12-17 01:58:40 +0300

    Add files via upload

---
+++ competitionManager-updeted/Competition.java
@@ -0,0 +1,63 @@
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public abstract class Competition {
+	
+	private String name;
+	private URL link;
+	private LocalDate dueDate;
+	
+	public Competition(String newName, URL newlink, LocalDate newDate) {
+		name = newName;
+		link = newlink;
+		dueDate = newDate;
+	}
+	
+	public abstract void deleteParticipant(Participant participant);
+
+	public abstract int getParticipantCount();
+
+	public abstract LinkedList<Participant> getParticipants();
+
+	public abstract void addParticipant(HashMap<String, String> args);
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public URL getLink() {
+		return link;
+	}
+
+	public void setLink(URL link) {
+		this.link = link;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+	public boolean equals(Competition competition) {
+		if (this.name.equals(competition.getName())) 
+			return true;
+		else 
+			return false;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " " + link.toString() + " " + dueDate.toString();
+	}
+	
+}
---
+++ competitionManager-updeted/Team.java
@@ -0,0 +1,61 @@
+
+import java.util.LinkedList;
+
+public class Team {
+
+	private static int teamCount = 0;
+	private String name;
+	private int number;
+	private LinkedList<Participant> participants = new LinkedList<Participant>();
+	private int rank;
+	
+	
+	public Team(String newName) {
+		teamCount++;
+		name = newName;
+		number = teamCount;
+	}
+
+
+	public void addParticipant(String name,String id,String major) {
+		participants.add(new Participant(name, id, major, this));
+	}
+	
+	public void deleteParticipant(int index) {
+		participants.remove(index);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getNumber() {
+		return number;
+	}
+
+
+	public LinkedList<Participant> getParticipants() {
+		return participants;
+	}
+
+
+	public int getRank() {
+		return rank;
+	}
+
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
